b935d0dc9b7ae9c68f020510e1534d9ff1d0cf83
Ryan Watkins <r_watkins@digitalfoundry.com>
2022-02-07 00:34:18 -0800

    Home page size updates

--- web/src/views/HomeView/HomeView.js
+++ web/src/views/HomeView/HomeView.js
@@ -10,14 +10,22 @@ import profileBlob from '../../assets/pictures/profileBlob.png'
 export const HomeView = () => {
   return (
     <Box className={styles.home}>
-      <Box>
+      <Box className={styles.profileContainer}>
         <img src={profileBlob} alt={"idk"} className={styles.profile}/>
       </Box>
 
       <Box className={styles.descriptionContainer}>
         <Stack spacing={2} direction='column' className={styles.description}>
           <Typography variant='h6' sx={{ color: '#a16a91' }}>Software Engineer</Typography>
-          <Typography variant='h3'>Hello, I'm Ryan</Typography>
+          <Typography
+            variant='h3'
+            sx={{
+              textAlign: 'left',
+              fontSize: '3vw',
+            }}
+          >
+            Hello, I'm Ryan Watkins
+          </Typography>
           <Typography
             variant='h6'
             sx={{
